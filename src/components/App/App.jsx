44ffaf9d0d6c0d0a2bf013fba7b9184a2480f295
Liza <yelyzaveta.mishchenko@gmail.com>
2025-04-10 16:42:12 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/contactsOps";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
 import styles from "./App.module.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,10 @@
 import Contact from "../Contact/Contact";
 import styles from "./ContactList.module.css";
 import { useSelector } from "react-redux";
+import { selectFilteredContacts } from "../../redux/contacts/selectors";
 
 export default function ContactList() {
-  const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.name);
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter)
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={styles.list}>
